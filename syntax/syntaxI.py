dc259e9fc6bff544431cdc0b1e63acc96c6f8d08
Brian Castro <camilitooso77@gmail.com>
2020-10-09 00:48:11 -0500

    Initializing course with python

---
+++ syntax/syntaxI.py
@@ -0,0 +1,28 @@
+"""Data types."""
+#Numbers: int, float.
+numberInt = 5
+numberFloat = 5.555
+
+#Strings
+textString = "Hello World!"
+
+#Booleans: true or false.
+myBoolVar = False
+
+"""Operators types."""
+#Aritmetics: +, -, *, /, %, //, **
+mySum = 5+5
+mySubtract = 5-5
+myMulti = 5*5
+myDivision = 5/5
+myModule = 10%3
+myExponent = 5**5
+myIntDivision = 9//2
+
+#Comparison: igual qué (==), diferente qué(!=), mayor qué(>), menor qué(<), mayor o igual qué(>=), menor o igual qué(<=).
+
+#Logics: AND, OR, NOT.
+
+#Asignment: igual(=), incremento(+=), decremento(-=), *=, /=, %=, //=, **=.
+
+#Specials: IS, IS NOT, IN, NOT IN.
